70976483b61193bc2650e22a6d562ca1d18f1024
Antonio Maiorano <amaiorano@gmail.com>
2015-05-21 20:46:12 -0400

    Add sample for InnerTransition

--- samples/hsm_wiki_samples/sample01/chapter3.cpp
+++ samples/hsm_wiki_samples/sample01/chapter3.cpp
@@ -1,4 +1,4 @@
-#define ENABLED_SECTION 3
+#define ENABLED_SECTION 4
 
 #if ENABLED_SECTION == 1
 
@@ -402,6 +402,107 @@ int main()
 	MyOwner myOwner;
 	myOwner.UpdateStateMachine();
 	myOwner.Die();
+	myOwner.UpdateStateMachine();
+}
+
+#elif ENABLED_SECTION == 4
+
+// inner_transition.cpp
+
+#include "hsm/statemachine.h"
+
+using namespace hsm;
+
+class MyOwner
+{
+public:
+	MyOwner();
+	void UpdateStateMachine();
+
+	void Die() { mDead = true; }
+	void SetMove(bool enable) { mMove = enable; }
+
+private:
+	bool IsDead() const { return mDead; }
+	bool PressedMove() const { return mMove; }
+
+	bool mDead;
+	bool mMove;
+
+	friend struct MyStates;
+	StateMachine mStateMachine;
+};
+
+struct MyStates
+{
+	struct BaseState : StateWithOwner<MyOwner>
+	{
+	};
+
+	struct Alive : BaseState
+	{
+		virtual Transition GetTransition()
+		{
+			if (Owner().IsDead())
+				return SiblingTransition<Dead>();
+
+			return InnerEntryTransition<Locomotion>();
+		}
+	};
+
+	struct Dead : BaseState
+	{
+		virtual Transition GetTransition()
+		{
+			return NoTransition();
+		}
+	};
+
+	struct Locomotion : BaseState
+	{
+		virtual Transition GetTransition()
+		{
+			if (Owner().PressedMove())
+				return InnerTransition<Move>();
+			else
+				return InnerTransition<Stand>();
+		}
+	};
+
+	struct Stand : BaseState
+	{
+	};
+
+	struct Move : BaseState
+	{
+	};
+};
+
+MyOwner::MyOwner()
+	: mDead(false)
+	, mMove(false)
+{
+	mStateMachine.Initialize<MyStates::Alive>(this);
+	mStateMachine.SetDebugInfo("TestHsm", TraceLevel::Basic);
+}
+
+void MyOwner::UpdateStateMachine()
+{
+	mStateMachine.ProcessStateTransitions();
+	mStateMachine.UpdateStates();
+}
+
+int main()
+{
+	MyOwner myOwner;
+	myOwner.UpdateStateMachine();
+
+	printf("Set Move = true\n");
+	myOwner.SetMove(true);
+	myOwner.UpdateStateMachine();
+
+	printf("Set Move = false\n");
+	myOwner.SetMove(false);
 	myOwner.UpdateStateMachine();
 }
 
