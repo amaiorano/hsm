5a5a1f0ad2e0106c9b77f0a96ca1f755be1efb3d
Antonio Maiorano <amaiorano@gmail.com>
2015-06-04 17:30:08 -0400

    Add Revisit UpdateStates sample

--- samples/hsm_wiki_samples/sample01/chapter3.cpp
+++ samples/hsm_wiki_samples/sample01/chapter3.cpp
@@ -1,4 +1,4 @@
-#define ENABLED_SECTION 4
+#define ENABLED_SECTION 5
 
 #if ENABLED_SECTION == 1
 
@@ -503,6 +503,97 @@ int main()
 
 	printf("Set Move = false\n");
 	myOwner.SetMove(false);
+	myOwner.UpdateStateMachine();
+}
+
+#elif ENABLED_SECTION == 5
+
+// revisit_update_states.cpp
+
+#include "hsm/statemachine.h"
+
+using namespace hsm;
+
+class MyOwner
+{
+public:
+	MyOwner();
+	void UpdateStateMachine();
+
+private:
+	friend struct MyStates;
+	StateMachine mStateMachine;
+};
+
+struct MyStates
+{
+	struct BaseState : StateWithOwner<MyOwner>
+	{
+	};
+	
+	struct A : BaseState
+	{
+		virtual Transition GetTransition()
+		{
+			return InnerEntryTransition<B>();
+		}
+
+		virtual void Update()
+		{
+			printf("A::Update\n");
+		}
+	};
+
+	struct B : BaseState
+	{
+		virtual Transition GetTransition()
+		{
+			return InnerEntryTransition<C>();
+		}
+
+		virtual void Update()
+		{
+			printf("B::Update\n");
+		}
+	};
+
+	struct C : BaseState
+	{
+		virtual Transition GetTransition()
+		{
+			return InnerEntryTransition<D>();
+		}
+
+		virtual void Update()
+		{
+			printf("C::Update\n");
+		}
+	};
+
+	struct D : BaseState
+	{
+		virtual void Update()
+		{
+			printf("D::Update\n");
+		}
+	};
+};
+
+MyOwner::MyOwner()
+{
+	mStateMachine.Initialize<MyStates::A>(this);
+	mStateMachine.SetDebugInfo("TestHsm", TraceLevel::Basic);
+}
+
+void MyOwner::UpdateStateMachine()
+{
+	mStateMachine.ProcessStateTransitions();
+	mStateMachine.UpdateStates();
+}
+
+int main()
+{
+	MyOwner myOwner;
 	myOwner.UpdateStateMachine();
 }
 
