8f93d97f4b56e26b96c1476b1dccb6290432a9c6
Antonio Maiorano <amaiorano@gmail.com>
2015-07-23 08:07:57 -0400

    Add samples for Sharing HSMs section

---
+++ samples/hsm_wiki_samples/source/ch4/shared_states_related_owners.cpp
@@ -0,0 +1,235 @@
+// shared_states_related_owners.cpp
+
+#include "hsm/statemachine.h"
+
+using namespace hsm;
+
+class AnimComponent
+{
+public:
+	AnimComponent() : mLoop(false) {}
+	void PlayAnim(const char* name, bool loop)
+	{
+		printf(">>> PlayAnim: %s, looping: %s\n", name, loop ? "true" : "false");
+		mLoop = loop;
+	}
+
+	bool IsFinished() const { return !mLoop; }
+
+private:
+	bool mLoop;
+};
+
+////////////////////// Character //////////////////////
+
+class Character
+{
+public:
+	void Update();
+
+protected:
+	friend struct SharedStates;
+	StateMachine mStateMachine;
+	AnimComponent mAnimComponent;
+};
+
+void Character::Update()
+{
+	// Update state machine
+	mStateMachine.ProcessStateTransitions();
+	mStateMachine.UpdateStates();
+}
+
+struct SharedStates
+{
+	// These states can be shared by state machines with Character-derived owners
+
+	struct BaseState : StateWithOwner<Character>
+	{
+	};
+
+	struct PlayAnim_Done : BaseState
+	{
+	};
+
+	struct PlayAnim : BaseState
+	{
+		struct Args : StateArgs
+		{
+			Args(const char* animName
+				, bool loop = true
+				, const Transition& doneTransition = SiblingTransition<PlayAnim_Done>()
+				)
+				: animName(animName)
+				, loop(loop)
+				, doneTransition(doneTransition)
+			{}
+
+			const char* animName;
+			bool loop;
+			Transition doneTransition;
+		};
+
+		virtual void OnEnter(const Args& args)
+		{
+			Owner().mAnimComponent.PlayAnim(args.animName, args.loop);
+			mDoneTransition = args.doneTransition;
+		}
+
+		virtual Transition GetTransition()
+		{
+			if (Owner().mAnimComponent.IsFinished())
+				return mDoneTransition;
+
+			return NoTransition();
+		}
+
+		Transition mDoneTransition;
+	};
+};
+
+////////////////////// Hero //////////////////////
+
+class Hero : public Character
+{
+public:
+	Hero();
+
+	// Public to simplify sample
+	bool mAttack;
+
+private:
+	friend struct HeroStates;
+};
+
+
+struct HeroStates
+{
+	struct BaseState : StateWithOwner<Hero, SharedStates::BaseState>
+	{
+	};
+
+	typedef SharedStates::PlayAnim_Done PlayAnim_Done;
+	typedef SharedStates::PlayAnim PlayAnim;
+
+	struct Alive : BaseState
+	{
+		virtual Transition GetTransition()
+		{
+			return InnerEntryTransition<Stand>();
+		}
+	};
+
+	struct Stand : BaseState
+	{
+		virtual Transition GetTransition()
+		{
+			if (Owner().mAttack)
+			{
+				Owner().mAttack = false;
+				return SiblingTransition<Attack>();
+			}
+
+			return NoTransition();
+		}
+	};
+
+	struct Attack : BaseState
+	{
+		virtual Transition GetTransition()
+		{
+			if (IsInInnerState<PlayAnim_Done>())
+				return SiblingTransition<Stand>();
+
+			return InnerEntryTransition<PlayAnim>(PlayAnim::Args("Attack_1", false));
+		}
+	};
+};
+
+Hero::Hero()
+	: mAttack(false)
+{
+	mStateMachine.Initialize<HeroStates::Alive>(this);
+	mStateMachine.SetDebugInfo("TestHsm", TraceLevel::Basic);
+}
+
+
+////////////////////// Enemy //////////////////////
+
+class Enemy : public Character
+{
+public:
+	Enemy();
+
+	// Public to simplify sample
+	bool mAttack;
+
+private:
+	friend struct EnemyStates;
+};
+
+struct EnemyStates
+{
+	struct BaseState : StateWithOwner<Enemy, SharedStates::BaseState>
+	{
+	};
+
+	typedef SharedStates::PlayAnim_Done PlayAnim_Done;
+	typedef SharedStates::PlayAnim PlayAnim;
+
+	struct Alive : BaseState
+	{
+		virtual Transition GetTransition()
+		{
+			return InnerEntryTransition<Stand>();
+		}
+	};
+
+	struct Stand : BaseState
+	{
+		virtual Transition GetTransition()
+		{
+			if (Owner().mAttack)
+			{
+				Owner().mAttack = false;
+				return SiblingTransition<Attack>();
+			}
+
+			return NoTransition();
+		}
+	};
+
+	struct Attack : BaseState
+	{
+		virtual Transition GetTransition()
+		{
+			if (IsInInnerState<PlayAnim_Done>())
+				return SiblingTransition<Stand>();
+
+			return InnerEntryTransition<PlayAnim>(PlayAnim::Args("Attack_1", false));
+		}
+	};
+};
+
+Enemy::Enemy()
+	: mAttack(false)
+{
+	mStateMachine.Initialize<EnemyStates::Alive>(this);
+	mStateMachine.SetDebugInfo("TestHsm", TraceLevel::Basic);
+}
+
+
+////////////////////// main //////////////////////
+
+int main()
+{
+	Hero hero;
+	hero.Update();
+	hero.mAttack = true;
+	hero.Update();
+
+	Enemy enemy;
+	enemy.Update();
+	enemy.mAttack = true;
+	enemy.Update();
+}
