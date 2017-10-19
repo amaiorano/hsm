amaiorano/hsm

commit 53d8e6da2dab03837558a2eb05340e6b23718a25

Always store RTTI info on StateTypeId, whether using C++ RTTI or custom
Also improve compiler definition macros.

include/hsm.h
@@ -18,7 +18,20 @@
 #ifndef __HSM_H__
 #define __HSM_H__
 
-#ifdef _MSC_VER
+// Compiler defines
+#if defined(__clang__)
+#define HSM_COMPILER_CLANG
+#elif defined(__GNUC__) || defined(__GNUG__)
+#define HSM_COMPILER_GCC
+#elif defined(_MSC_VER)
+#define HSM_COMPILER_MSC
+#endif
+
+#if defined(HSM_COMPILER_CLANG) || defined(HSM_COMPILER_GCC)
+#define HSM_COMPILER_CLANG_OR_GCC
+#endif
+
+#ifdef HSM_COMPILER_MSC
 #pragma region "Config"
 #endif
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -41,12 +54,11 @@
 #endif
 #endif
 
-// If set, even if the compiler has RTTI enabled, this will force the usage of the custom HSM RTTI system
-#define HSM_FORCE_CUSTOM_RTTI 0
-
-// HSM_CPP_RTTI is set if the compiler has standard C++ RTTI support enabled
-#if !HSM_FORCE_CUSTOM_RTTI && ( (defined(_MSC_VER) && defined(_CPPRTTI)) || (defined(__GNUC__) && defined(__GXX_RTTI)) )
-#define HSM_CPP_RTTI 1
+// If set and C++ RTTI is enabled, will use C++ RTTI instead of the custom HSM RTTI system to
+// identify state types and return state names. Using C++ RTTI may yield better performance
+// when comparing StateTypeIds, but the state names returned are usually less human readable.
+#if !defined(HSM_USE_CPP_RTTI_IF_ENABLED)
+#define HSM_USE_CPP_RTTI_IF_ENABLED 1
 #endif
 
 #define HSM_STD_VECTOR std::vector
@@ -71,13 +83,13 @@ typedef char hsm_char;
 #define HSM_PRINTF ::printf
 #define STRCMP ::strcmp
 
-#ifdef _MSC_VER
+#ifdef HSM_COMPILER_MSC
 #define SNPRINTF ::_snprintf_s
 #else
 #define SNPRINTF ::snprintf
 #endif
 
-#ifdef _MSC_VER
+#ifdef HSM_COMPILER_MSC
 #define STRNCPY ::strncpy_s
 #else
 #define STRNCPY ::strncpy
@@ -101,18 +113,23 @@ typedef void Owner;
 
 } // namespace hsm
 
-#ifdef _MSC_VER
+#ifdef HSM_COMPILER_MSC
 #pragma endregion "Config"
 #endif
 
-#ifdef _MSC_VER
+#ifdef HSM_COMPILER_MSC
 #pragma region "RTTI"
 #endif
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // RTTI
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-#if HSM_CPP_RTTI
+// HSM_USE_CPP_RTTI is set if the compiler has standard C++ RTTI support enabled
+#if HSM_USE_CPP_RTTI_IF_ENABLED && ( (defined(HSM_COMPILER_MSC) && defined(_CPPRTTI)) || (defined(HSM_COMPILER_CLANG_OR_GCC) && defined(__GXX_RTTI)) )
+#define HSM_USE_CPP_RTTI
+#endif
+
+#ifdef HSM_USE_CPP_RTTI
 
 #include <typeinfo>
 
@@ -121,35 +138,34 @@ namespace hsm {
 // We use standard C++ RTTI
 
 // We need a copyable wrapper around std::type_info since std::type_info is non-copyable
-struct StateTypeIdStorage
+struct StateTypeId
 {
-	StateTypeIdStorage() : m_typeInfo(0) { }
-	StateTypeIdStorage(const std::type_info& typeInfo) { m_typeInfo = &typeInfo; }
-	hsm_bool operator==(const StateTypeIdStorage& rhs) const
+	StateTypeId() : mTypeInfo(0) {}
+	StateTypeId(const std::type_info& typeInfo) : mTypeInfo(&typeInfo) {}
+	hsm_bool operator==(const StateTypeId& rhs) const
 	{
-		HSM_ASSERT_MSG(m_typeInfo != 0, "m_typeInfo was not properly initialized");
-		return *m_typeInfo == *rhs.m_typeInfo;
+		HSM_ASSERT_MSG(mTypeInfo != 0, "mTypeInfo was not properly initialized");
+		return *mTypeInfo == *rhs.mTypeInfo;
 	}
-	const std::type_info* m_typeInfo;
+	const std::type_info* mTypeInfo;
 };
 
-typedef const StateTypeIdStorage& StateTypeId;
+template <typename StateType>
+const StateTypeId& GetStateType()
+{
+	static StateTypeId stateTypeId(typeid(StateType));
+	return stateTypeId;
+}
 
 template <typename StateType>
-StateTypeId GetStateType()
+const char* GetStateName()
 {
-	static StateTypeIdStorage stateTypeId(typeid(StateType));
-	return stateTypeId;
+	return GetStateType<StateType>().mTypeInfo->name();
 }
 
 } // namespace hsm
 
-// DEFINE_HSM_STATE is NOT necessary; however, we define it here to nothing to make it easier to
-// test switching between compiler RTTI enabled or disabled.
-#define DEFINE_HSM_STATE(__StateName__)
-
-
-#else // !HSM_CPP_RTTI
+#else // !HSM_USE_CPP_RTTI
 
 namespace hsm {
 
@@ -157,24 +173,30 @@ namespace hsm {
 // DEFINE_HSM_STATE macro, which makes use of the input name of the state as the unique identifier. String
 // compares are used to determine equality.
 
-// Like std::type_info, we need to be able test equality and get a unique name
-struct StateTypeIdStorage
+// We need a comparable wrapper around the type name. We can't just compare const char* pointers
+// because GetTypeName<T> may return two different strings for the same T in different translation units.
+struct StateTypeId
 {
-	StateTypeIdStorage(const hsm_char* aStateName = 0) : mStateName(aStateName) {}
-	hsm_bool operator==(const StateTypeIdStorage& rhs) const
+	StateTypeId(const hsm_char* aStateName = 0) : mStateName(aStateName) {}
+	hsm_bool operator==(const StateTypeId& rhs) const
 	{
 		HSM_ASSERT_MSG(mStateName != 0, "StateTypeId was not properly initialized");
 		return STRCMP(mStateName, rhs.mStateName) == 0;
 	}
 	const hsm_char* mStateName;
 };
-
-typedef const StateTypeIdStorage& StateTypeId;
+	
+template <typename StateType>
+const StateTypeId& GetStateType()
+{
+	static StateTypeId stateTypeId = internal::GetTypeName<StateType>();
+	return stateTypeId;
+}
 
 template <typename StateType>
-StateTypeId GetStateType()
+const char* GetStateName()
 {
-	return StateType::GetStaticStateType();
+	return GetStateType().mStateName;
 }
 
 } // namespace hsm
@@ -185,13 +207,13 @@ StateTypeId GetStateType()
 	virtual hsm::StateTypeId DoGetStateType() const { return GetStaticStateType(); } \
 	virtual const hsm_char* DoGetStateDebugName() const { return HSM_TEXT(#__StateName__); }
 
-#endif // !HSM_CPP_RTTI
+#endif // !HSM_USE_CPP_RTTI
 
-#ifdef _MSC_VER
+#ifdef HSM_COMPILER_MSC
 #pragma endregion "RTTI"
 #endif
 
-#ifdef _MSC_VER
+#ifdef HSM_COMPILER_MSC
 #pragma region "Transition"
 #endif
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -212,6 +234,7 @@ const StateFactory& GetStateFactory();
 struct StateFactory
 {
 	virtual StateTypeId GetStateType() const = 0;
+	virtual const char* GetStateName() const = 0;
 	virtual State* AllocateState() const = 0;
 };
 
@@ -226,6 +249,11 @@ struct ConcreteStateFactory : StateFactory
 	virtual StateTypeId GetStateType() const
 	{
 		return hsm::GetStateType<TargetState>();
+	}
+
+	virtual const char* GetStateName() const
+	{
+		return hsm::GetStateName<TargetState>();
 	}
 
 	virtual State* AllocateState() const
@@ -441,11 +469,11 @@ inline Transition NoTransition()
 
 } // namespace hsm
 
-#ifdef _MSC_VER
+#ifdef HSM_COMPILER_MSC
 #pragma endregion "Transition"
 #endif
 
-#ifdef _MSC_VER
+#ifdef HSM_COMPILER_MSC
 #pragma region "State"
 #endif
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -517,7 +545,7 @@ struct ConcreteStateValueResetter : StateValueResetter
 
 namespace detail
 {
-	void InitState(State* state, StateMachine* ownerStateMachine, size_t stackDepth);
+	void InitState(State* state, StateMachine* ownerStateMachine, size_t stackDepth, const StateFactory& stateFactory);
 }
 
 struct State
@@ -644,17 +672,7 @@ struct State
 	StateOverride<SourceState> GetStateOverride();
 
 private:
-
-#if HSM_CPP_RTTI
-	StateTypeIdStorage DoGetStateType() const { return typeid(*this); }
-	const hsm_char* DoGetStateDebugName() const { return typeid(*this).name(); }
-#else
-	// These are implemented in each state via the DEFINE_HSM_STATE macro
-	virtual StateTypeId DoGetStateType() const = 0;
-	virtual const hsm_char* DoGetStateDebugName() const = 0;
-#endif
-
-	friend void detail::InitState(State* state, StateMachine* ownerStateMachine, size_t stackDepth);
+	friend void detail::InitState(State* state, StateMachine* ownerStateMachine, size_t stackDepth, const StateFactory& stateFactory);
 
 	template <typename T>
 	StateValue<T>* FindStateValueInResetterList(StateValue<T>& stateValue)
@@ -691,7 +709,7 @@ private:
 	StateValueResetterList mStateValueResetters;
 
 	// Values cached to avoid virtual call, especially since the values are constant
-	StateTypeIdStorage mStateTypeId;
+	StateTypeId mStateTypeId;
 	const hsm_char* mStateDebugName;
 };
 
@@ -729,11 +747,11 @@ private:
 
 } // namespace hsm
 
-#ifdef _MSC_VER
+#ifdef HSM_COMPILER_MSC
 #pragma endregion "State"
 #endif
 
-#ifdef _MSC_VER
+#ifdef HSM_COMPILER_MSC
 #pragma region "StateMachine"
 #endif
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -1017,20 +1035,20 @@ inline const StateFactory& StateMachine::GetStateOverride()
 
 namespace detail
 {
-	inline void InitState(State* state, StateMachine* ownerStateMachine, size_t stackDepth)
+	inline void InitState(State* state, StateMachine* ownerStateMachine, size_t stackDepth, const StateFactory& stateFactory)
 	{
 		HSM_ASSERT(ownerStateMachine != 0);
 		state->mOwnerStateMachine = ownerStateMachine;
 		state->mOwner = ownerStateMachine->GetOwner();
 		state->mStackDepth = stackDepth;
-		state->mStateTypeId = state->DoGetStateType();
-		state->mStateDebugName = state->DoGetStateDebugName();
+		state->mStateTypeId = stateFactory.GetStateType();
+		state->mStateDebugName = stateFactory.GetStateName();
 	}
 
 	inline State* CreateState(const Transition& transition, StateMachine* ownerStateMachine, size_t stackDepth)
 	{
 		State* state = transition.GetStateFactory().AllocateState();
-		InitState(state, ownerStateMachine, stackDepth);
+		InitState(state, ownerStateMachine, stackDepth, transition.GetStateFactory());
 		return state;
 	}
 
@@ -1347,7 +1365,7 @@ inline void StateMachine::LogTransition(size_t minLevel, size_t depth, const hsm
 
 } // namespace hsm
 
-#ifdef _MSC_VER
+#ifdef HSM_COMPILER_MSC
 #pragma endregion "StateMachine"
 #endif
 

commit 0ee426b6492085bc614a3aa6f531e4def6de60ac

Fix support of non-RTTI compilation
Regression was introduced in 53d8e6da2dab03837558a2eb05340e6b23718a25

include/hsm.h
@@ -175,6 +175,10 @@ const char* GetStateName()
 
 } // namespace hsm
 
+// DEFINE_HSM_STATE is NOT necessary; however, we define it here to nothing to make it easier to
+// test switching between compiler RTTI enabled or disabled.
+#define DEFINE_HSM_STATE(__StateName__)
+
 #else // !HSM_USE_CPP_RTTI
 
 namespace hsm {
@@ -197,23 +201,22 @@ struct StateTypeId
 };
 	
 template <typename StateType>
-const StateTypeId& GetStateType()
+StateTypeId GetStateType()
 {
-	static StateTypeId stateTypeId = internal::GetTypeName<StateType>();
-	return stateTypeId;
+	return StateType::GetStaticStateType();
 }
 
 template <typename StateType>
 const char* GetStateName()
 {
-	return GetStateType().mStateName;
+	return GetStateType<StateType>().mStateName;
 }
 
 } // namespace hsm
 
 // Must use this macro in every State to add RTTI support.
 #define DEFINE_HSM_STATE(__StateName__) \
-	static hsm::StateTypeId GetStaticStateType() { static hsm::StateTypeIdStorage sStateTypeId(HSM_TEXT(#__StateName__)); return sStateTypeId; } \
+	static hsm::StateTypeId GetStaticStateType() { static hsm::StateTypeId sStateTypeId(HSM_TEXT(#__StateName__)); return sStateTypeId; } \
 	virtual hsm::StateTypeId DoGetStateType() const { return GetStaticStateType(); } \
 	virtual const hsm_char* DoGetStateDebugName() const { return HSM_TEXT(#__StateName__); }
 

samples/hsm_book_samples/source/ch2/improving_readability.cpp
@@ -1,5 +1,7 @@
 // improving_readability.cpp
 
+#define HSM_USE_CPP_RTTI_IF_ENABLED 1
+
 #include "hsm.h"
 
 using namespace hsm;
@@ -8,6 +10,8 @@ struct MyStates
 {
 	struct First : State
 	{
+		DEFINE_HSM_STATE(First);
+
 		virtual Transition GetTransition()
 		{
 			return SiblingTransition<Second>();
@@ -16,6 +20,8 @@ struct MyStates
 
 	struct Second : State
 	{
+		DEFINE_HSM_STATE(Second);
+
 		virtual Transition GetTransition()
 		{
 			return SiblingTransition<Third>();
@@ -24,6 +30,8 @@ struct MyStates
 
 	struct Third : State
 	{
+		DEFINE_HSM_STATE(Third);
+
 		virtual Transition GetTransition()
 		{
 			return NoTransition();